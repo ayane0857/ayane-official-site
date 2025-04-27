f0bf6460e9db461ee9fd7b62d65c1dccc1556703
Shinahin <tailangrongyi@gmail.com>
2025-04-28 02:06:36 +0900

    update ui and text

--- app/page.tsx
+++ app/page.tsx
@@ -2,66 +2,84 @@ import Image from "next/image";
 import { Card } from "@/components/ui/card";
 import { FaGithub, FaTwitter, FaYoutube, FaDiscord } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
+import { MdGroups } from "react-icons/md";
 
 export default async function Home() {
   return (
     <div className="flex flex-col md:flex-row min-h-screen w-full bg-[#F9F6F7] p-4 md:p-8">
-      <div className="w-full md:w-64 lg:w-72 flex items-center justify-center md:justify-start mb-6 md:mb-0">
+      <div className="w-full md:w-80 lg:w-96 flex items-center justify-center md:justify-start mb-6 md:mb-0">
         <Card className="w-full h-auto flex flex-col items-center bg-[#D58F99] shadow-lg border-0 p-4">
           <div className="flex flex-col items-center w-full h-full space-y-4">
-            <div className="w-32 h-32 relative">
-              <Image
-                src="/icon.png"
-                alt="アイコン"
-                width={128}
-                height={128}
-                className="object-cover"
-              />
-            </div>
-            <h2 className="text-xl md:text-2xl font-bold">彩音</h2>
-            <div className="flex space-x-4 my-2">
-              <a
-                href="https://github.com/ayane0857"
-                aria-label="GitHub"
-                className="hover:opacity-80"
-              >
-                <FaGithub className="w-5 h-5 md:w-6 md:h-6" />
-              </a>
-              <a
-                href="https://x.com/ayane0857_"
-                aria-label="Twitter"
-                className="hover:opacity-80"
-              >
-                <FaTwitter className="w-5 h-5 md:w-6 md:h-6" />
-              </a>
-              <a
-                href="https://www.youtube.com/@ayane0857"
-                aria-label="YouTube"
-                className="hover:opacity-80"
-              >
-                <FaYoutube className="w-5 h-5 md:w-6 md:h-6" />
-              </a>
-              <a
-                href="https://discord.gg/qyTSUJFctN"
-                aria-label="Discord"
-                className="hover:opacity-80"
-              >
-                <FaDiscord className="w-5 h-5 md:w-6 md:h-6" />
-              </a>
-            </div>
-            <div className="flex items-center space-x-2">
-              <a
-                href="mailto:info@ayane0857.net"
-                className="flex items-center space-x-1 text-sm md:text-base hover:opacity-80"
-              >
-                <MdEmail className="w-4 h-4 md:w-5 md:h-5" />
-                <span className="break-all">info@ayane0857.net</span>
-              </a>
+            <div className="flex justify-between items-center">
+              <div className="w-32 h-32 relative">
+                <Image
+                  src="/icon.png"
+                  alt="アイコン"
+                  width={128}
+                  height={128}
+                  className="object-cover"
+                />
+              </div>
+              <div className="">
+                <h2 className="text-xl md:text-2xl font-bold">彩音</h2>
+                <div className="flex space-x-4 my-2">
+                  <a
+                    href="https://github.com/ayane0857"
+                    aria-label="GitHub"
+                    className="hover:opacity-80"
+                  >
+                    <FaGithub className="w-5 h-5 md:w-6 md:h-6" />
+                  </a>
+                  <a
+                    href="https://x.com/ayane0857_"
+                    aria-label="Twitter"
+                    className="hover:opacity-80"
+                  >
+                    <FaTwitter className="w-5 h-5 md:w-6 md:h-6" />
+                  </a>
+                  <a
+                    href="https://www.youtube.com/@ayane0857"
+                    aria-label="YouTube"
+                    className="hover:opacity-80"
+                  >
+                    <FaYoutube className="w-5 h-5 md:w-6 md:h-6" />
+                  </a>
+                  <a
+                    href="https://discord.gg/qyTSUJFctN"
+                    aria-label="Discord"
+                    className="hover:opacity-80"
+                  >
+                    <FaDiscord className="w-5 h-5 md:w-6 md:h-6" />
+                  </a>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <a
+                    href="mailto:info@ayane0857.net"
+                    className="flex items-center space-x-1 text-sm md:text-base hover:opacity-80"
+                  >
+                    <MdEmail className="w-4 h-4 md:w-5 md:h-5" />
+                    <span className="break-all">info@ayane0857.net</span>
+                  </a>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <a
+                    href="https://www.uniproject.jp/"
+                    className="flex items-center space-x-1 text-sm md:text-base hover:opacity-80"
+                  >
+                    <MdGroups className="w-4 h-4 md:w-5 md:h-5" />
+                    <span className="break-all">UniProject</span>
+                  </a>
+                </div>
+              </div>
             </div>
             <p className="text-ml leading-relaxed">
-              そこらへんにいる一般階級インフラレッドのぬっこ
+              そこらへんにいる一般階級インフラレッドのぬっこです
               <br />
-              動画編集やプログラミングとか暇があったらやってる
+              動画投稿してるけど、最近あんまりやってない
+              <br />
+              プログラミングとか暇があったらやってる
+              <br />
+              UniProjectに所属している
             </p>
             <div className="flex flex-col space-y-2 w-full items-center">
               <p className="text-lg">相互リンク</p>
@@ -94,7 +112,7 @@ export default async function Home() {
         </Card>
       </div>
 
-      <div className="w-full md:w-[calc(100%-16rem)] lg:w-[calc(100%-18rem)] flex items-center justify-start p-4 md:p-12 bg-slate-50 dark:bg-slate-900">
+      <div className="w-full md:w-[calc(100%-20rem)] lg:w-[calc(100%-24rem)] flex items-center justify-start p-4 md:p-12 bg-slate-50 dark:bg-slate-900">
         <div className="max-w-4xl w-full mx-auto">
           <h1 className="text-4xl font-bold mb-8 pb-3 border-b flex items-center gap-2">
             <span className="text-primary">運営サービス一覧</span>
