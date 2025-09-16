9a95b11167831d7dfae0c2c9eac385dd337b6356
Shinahin <tailangrongyi@gmail.com>
2025-09-17 00:30:55 +0900

    いらない設定を修正

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    MICROCMS_SERVICE_DOMAIN: process.env.MICROCMS_SERVICE_DOMAIN,
-    MICROCMS_API_KEY: process.env.MICROCMS_API_KEY,
+  experimental: {
+    optimizeCss: true,
   },
 };
 
