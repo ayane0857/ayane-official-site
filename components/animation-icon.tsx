82101830dc4b33f481254553576225543e7ccfd9
Shinahin <tailangrongyi@gmail.com>
2025-09-20 18:48:30 +0900

    撫でられるように進化しちまったぜ☆

--- components/animation-icon.tsx
+++ components/animation-icon.tsx
@@ -1,30 +1,38 @@
 "use client";
 
 import { animate, hover, press } from "motion";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 export const Icon = () => {
+  const [isteretere, setIsteretere] = useState(false);
   useEffect(() => {
     hover(".icon", (element) => {
+      setIsteretere(true);
       animate(element, { scale: 1.1, rotate: -10 }, { duration: 0.3 });
 
       return () => {
+        setIsteretere(false);
         animate(element, { scale: 1, rotate: 0 }, { duration: 0.3 });
       };
     });
     press(".icon", (element) => {
+      setIsteretere(true);
       animate(element, { scale: 0.9 }, { type: "spring", stiffness: 700 });
 
-      return () =>
+      return () => {
+        setTimeout(() => {
+          setIsteretere(false);
+        }, 500);
         animate(element, { scale: 1 }, { type: "spring", stiffness: 500 });
+      };
     });
   }, []);
 
   return (
     <div className="w-38 h-38 relative cursor-grab icon">
       <Image
-        src="/icon.png"
+        src={isteretere ? "/teretere.png" : "/icon.png"}
         alt="アイコン"
         width={192}
         height={192}
