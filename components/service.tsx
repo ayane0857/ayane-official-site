3db9394103ae28408f935ba3e6dcc689fe0ab208
Shinahin <tailangrongyi@gmail.com>
2025-10-05 20:36:49 +0900

    彩音のスライド公開所を追加

--- components/service.tsx
+++ components/service.tsx
@@ -79,6 +79,42 @@ export const Service = () => {
           </a>
         </div>
       </AnimationServiceWrapper>
+      <AnimationServiceWrapper>
+        <div className="flex flex-col mb-8 pb-3 gap-6">
+          <a
+            href="https://slides.ayane0857.net"
+            className="block group transition-all duration-300"
+          >
+            <div className="border rounded-lg overflow-hidden shadow-sm duration-300 bg-white dark:bg-slate-800 hover:shadow-md">
+              <div className="p-6">
+                <h2 className="text-2xl font-bold mb-2 text-gray-800 dark:text-gray-200 transition-colors">
+                  彩音のスライド公開所
+                </h2>
+                <p className="text-gray-600 dark:text-gray-400">
+                  私が今までにLTなどで使ったスライドを載せています
+                </p>
+                <div className="mt-4 flex items-center text-sm text-primary">
+                  <span>詳細を見る</span>
+                  <svg
+                    className="w-4 h-4 ml-1 group-hover:translate-x-1 transition-transform"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M9 5l7 7-7 7"
+                    ></path>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </AnimationServiceWrapper>
     </div>
   );
 };
